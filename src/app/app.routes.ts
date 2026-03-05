2c885dba2ad89551110b37ec2986d6f67fe10155
Tim Hudoba <timhudoba@gmail.com>
2026-03-05 13:45:54 -0600

    Added routing to the angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Details } from './details/details';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home page',
+    },
+    {
+        path: 'details',
+        component: Details,
+        title: 'Home Details'
+    },
+];
